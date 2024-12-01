e8a3593a04124d3ae5fbb980e82196a49990a6f5
Taavi Repän <trepan@gmail.com>
2024-12-01 09:17:05 +0200

    Day 1, Task 2

--- 01/Task1.java
+++ 01/Task1.java
@@ -6,6 +6,7 @@ class Task1 {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
+        int rcount[] = new int[100000];
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] elements = line.split("   ");
@@ -13,14 +14,18 @@ class Task1 {
             int num2 = Integer.parseInt(elements[1]); 
             left.add(num1);
             right.add(num2);
+            rcount[num2]++;
         }
         scanner.close();
         left.sort(Integer::compareTo);
         right.sort(Integer::compareTo);
-        int ret = 0;
+        int task1 = 0;
+        int task2 = 0;
         for (int i = 0; i < left.size(); i++) {
-            ret += Math.abs(left.get(i) - right.get(i));
+            task1 += Math.abs(left.get(i) - right.get(i));
+            task2 += left.get(i) * (rcount[left.get(i)]);
         }
-        System.out.println(ret);
+        System.out.println(task1);
+        System.out.println(task2);
     }
 }
