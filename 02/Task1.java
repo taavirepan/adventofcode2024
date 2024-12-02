382f2f6d2d33e12d92a6484a1955fdb1b919e751
Taavi Repän <trepan@gmail.com>
2024-12-02 07:30:57 +0200

    Day 2, task 2

--- 02/Task1.java
+++ 02/Task1.java
@@ -10,21 +10,49 @@ class Task1 {
         return ret;
     }
 
+    public static int[] delta2(int[] numbers, int skip) {
+        int[] ret = new int[numbers.length - 1];
+        for (int i = 0; i < ret.length; i++) {
+            if (i >= skip) {
+                ret[i] = numbers[i + 1];
+            }
+            else {
+                ret[i] = numbers[i];
+            }
+        }
+        return delta(ret);
+    }
+
+    public static boolean task1(int[] deltas) {
+        return Arrays.stream(deltas).allMatch(d -> d > 0 && d < 4) || Arrays.stream(deltas).allMatch(d -> d > -4 && d < 0);
+    }
+
+    public static boolean task2(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (task1(delta2(numbers, i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int ret = 0;
+        int ret1 = 0;
+        int ret2 = 0;
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] parts = line.split(" ");
             int[] numbers = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
-            int[] deltas = delta(numbers);
-            if (Arrays.stream(deltas).allMatch(d -> d > 0 && d < 4)) {
-                ret ++;
+            if (task1(delta(numbers))) {
+                ret1 ++;
+                ret2 ++;
             }
-            else if (Arrays.stream(deltas).allMatch(d -> d > -4 && d < 0)) {
-                ret ++;
+            else if (task2(numbers)) {
+                ret2 ++;
             }
         }
-        System.out.println(ret);
+        System.out.println(ret1);
+        System.out.println(ret2);
     }
 }
