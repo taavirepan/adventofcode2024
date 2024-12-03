4dedf25df32b1586049c0f70e4f7cabe96d9cac3
Taavi Repän <trepan@gmail.com>
2024-12-03 07:25:14 +0200

    Day 3, part 2

--- 03/Task1.java
+++ 03/Task1.java
@@ -6,14 +6,23 @@ class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+        Pattern pdont = Pattern.compile("do(n't)?\\(\\)");
 
         int task1 = 0;        
-        int i = 0;
+        boolean state = true;
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
+            int i = 0;
             Matcher matcher = pattern.matcher(line);
             while (matcher.find()) {
+                Matcher matcher2 = pdont.matcher(line.substring(i, matcher.start()));
                 i = matcher.end();
+                while (matcher2.find()) {
+                    state = matcher2.group().equals("do()");
+                }
+                if (!state) {
+                    continue;
+                }
                 int a = Integer.parseInt(matcher.group(1));
                 int b = Integer.parseInt(matcher.group(2));
                 task1 += a * b;
