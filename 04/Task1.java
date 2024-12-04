fe05820988a87889fdefeedacfe914b3e0b1d62f
Taavi Repän <trepan@gmail.com>
2024-12-04 07:31:28 +0200

    Day 4, task 2

--- 04/Task1.java
+++ 04/Task1.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 class Task1 {
-    static String substring(String[] data, int i, int j, int di, int dj) {
+    static String substring(String[] data, int i, int j, int di, int dj, int len) {
         StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < 4; k++) {
+        for (int k = 0; k < len; k++) {
             if (i + k * di < 0 || i + k * di >= data.length || j + k * dj < 0 || j + k * dj >= data[i + k * di].length()) {
                 break;
             }
@@ -12,6 +12,15 @@ class Task1 {
         return sb.toString();
     }
     
+    static boolean find_pattern_2(String[] data, int i, int j) {
+        boolean diag[] = new boolean[2];
+        for (int k = 0; k < 2; k++) {
+            diag[k] = substring(data, i, j + 2*k, 1, 1 - k*2, 3).equals("SAM");
+            diag[k] |= substring(data, i, j + 2*k, 1, 1 - k*2, 3).equals("MAS");
+        }
+        return diag[0] && diag[1];
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] lines = scanner.useDelimiter("\n").tokens().toArray(String[]::new);
@@ -21,16 +30,21 @@ class Task1 {
         int dj[] = {0, 1, 0, -1, 1, -1, 1, -1};
 
         int task1 = 0;
+        int task2 = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 for (int k = 0; k < 8; k++) {
-                    if (substring(lines, i, j, di[k], dj[k]).equals("XMAS")) {
+                    if (substring(lines, i, j, di[k], dj[k], 4).equals("XMAS")) {
                         task1++;
                     }
+                }
+                if (find_pattern_2(lines, i, j)) {
+                    task2++;
                 }
             }
         }
 
         System.out.println(task1);
+        System.out.println(task2);
     }
 }
