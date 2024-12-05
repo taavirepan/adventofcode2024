91bc9494c35b25ca8433af63648104ef98663904
Taavi Repän <trepan@gmail.com>
2024-12-05 07:41:17 +0200

    Day 5, task 2

--- 05/Task1.java
+++ 05/Task1.java
@@ -3,6 +3,28 @@ import java.util.List;
 import java.util.Scanner;
 
 class Task1 {
+    static List<Integer> reorder(List<Integer> numbers, List<Integer> rules1, List<Integer> rules2) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+
+    static int[][] processRules(List<Integer> rules1, List<Integer> rules2) {
+        int table[][] = new int[100][100];
+        for (int i = 0; i < rules1.size(); i++) {
+            int a = rules1.get(i);
+            int b = rules2.get(i);
+            table[a][b] = 1;
+            table[b][a] = -1;
+        }
+        return table;
+    }
+
+    static int solve_task2(List<Integer> numbers, int[][] sorting) {
+        numbers.sort((a, b) -> sorting[a][b]);
+        System.out.println(numbers);
+        return numbers.get(numbers.size() / 2);
+    }
+
     public static void main(String[] args) {
         List<Integer> rules1 = new ArrayList<>();
         List<Integer> rules2 = new ArrayList<>();
@@ -18,6 +40,8 @@ class Task1 {
         }
 
         int task1 = 0;
+        int task2 = 0;
+        int[][] sorting = processRules(rules1, rules2);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] parts = line.split(",");
@@ -40,7 +64,11 @@ class Task1 {
             if (valid) {
                 task1 += numbers.get(numbers.size() / 2);
             }
+            else {
+                task2 += solve_task2(numbers, sorting);
+            }
         }
         System.out.println(task1);
+        System.out.println(task2);
     }
 }
