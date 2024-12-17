02a4f3385b8d57b5a9421d40afa6506214d4f6d2
Taavi Repän <trepan@gmail.com>
2024-12-17 19:44:30 +0200

    Brute force doesn't work

--- 17/Task1.java
+++ 17/Task1.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
+import java.util.function.IntPredicate;
 import java.util.regex.*;
 
 class Task1 {
-    static String run(List<Integer> numbers) {
+    static boolean run(List<Integer> numbers, IntPredicate predicate) {
         int[] registers = numbers.subList(0, 3).stream().mapToInt(Integer::intValue).toArray();
         int[] program = numbers.subList(3, numbers.size()).stream().mapToInt(Integer::intValue).toArray();
-        StringBuilder result = new StringBuilder();
 
         int ip = 0;
         while (ip < program.length) {
@@ -27,14 +27,13 @@ class Task1 {
                 case 2 -> {registers[1] = combo % 8;}
                 case 3 -> {ip = (registers[0] != 0) ? literal - 2 : ip;}
                 case 4 -> {registers[1] = registers[1] ^ registers[2];}
-                case 5 -> {result.append(combo % 8 + ",");}
+                case 5 -> {if (!predicate.test(combo % 8)) {return false;}}
 
                 default -> throw new RuntimeException("Unknown opcode: " + opcode);
             }
             ip += 2;
         }
-
-        return result.toString().substring(0, result.length() - 1);
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -48,6 +47,24 @@ class Task1 {
                 numbers.add(Integer.parseInt(matcher.group()));
             }
         }
-        System.out.println(run(numbers));
+        {
+            List<Integer> result = new ArrayList<>();
+            run(numbers, i -> {result.add(i); return true;});
+            System.out.println(String.join(",", result.stream().map(Object::toString).toArray(String[]::new)));
+        }
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; ; i++) {
+            int[] j = {3};
+            numbers.set(0, i);
+            if (run(numbers, x -> j[0] == numbers.size() || numbers.get(j[0]++) == x) && j[0] == numbers.size()) {
+                System.out.println("Task2=" + i);
+                break;
+            }
+            if (i > 0 && i % 10000000 == 0) {
+                long secondsElapsed = (System.currentTimeMillis() - startTime) / 1000;
+                System.out.println("Iteration: " + i / 1000000 + "M, Seconds Elapsed: " + secondsElapsed);
+            }
+        }
     }
 }
